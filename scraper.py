947d5d78fd23787f628b07839ef2086745272650
Asko24 <askovsky24@gmail.com>
2020-04-08 13:35:53 +0200

    PG 2020-04-08

--- scraper.py
+++ scraper.py
@@ -29,6 +29,15 @@ selectors = {
     "review_date":['span.review-time > time:nth-of-type(2)',"datetime"]
 }
 
+#funkcja do usuwania znaków formatujących
+def remove_whitespaces(text):
+    try:
+        for char in ["\n", "\r"]:
+            text = text.replace(char, ". ")
+        return text
+    except AttributeError:
+        pass
+
 #adres URL strony z opiniami
 url_prefix = "https://www.ceneo.pl"
 product_id = input("Podaj kod produktu: ")
@@ -46,14 +55,20 @@ while url is not None:
     #wybranie z kodu strony fragmentów odpowiadających poszczególnym opiniom
     opinions = page_tree.select("li.js_product-review")
     
-    #ekstrakcja składowych dla pojedynczej opinii z listy
+    #ekstrakcja składowyh dla pojedynczej opinii z listy
     for opinion in opinions: 
       
         features = {key:extract_feature(opinion, *args)
                     for key, args in selectors.items()}
         features["opinion_id"] = int(opinion["data-entry-id"])
         features["purchased"] = True if features["purchased"] == "Opinia potwierdzona zakupem" else False
+        features["useful"] = int(features["useful"])
+        features["useless"] = int(features["useless"])
+        features["content"] = remove_whitespaces(features["content"])
+        features["pros"] = remove_whitespaces(features["pros"])
+        features["cons"] = remove_whitespaces(features["cons"])
         
+
         opinions_list.append(features)
 
     try:
@@ -63,6 +78,5 @@ while url is not None:
 
     print("url:",url)
 
-with open(product_id+".json", 'w', encoding="UTF-8") as fp:
+with open("opinions/"+product_id+".json", 'w', encoding="UTF-8") as fp:
     json.dump(opinions_list, fp, ensure_ascii=False, separators=(",",": "), indent=4 )
-
